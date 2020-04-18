00055d2b0dc3b8d95870ea2f3595e620eef17e35
Captain Fantasy <captain@fantasyui.com>
2020-04-18 16:12:13 -0400

    New Release

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const naturalComparator = require('natural-compare');
 module.exports = function(key){
-  return function (a,b){
-    let A = a[key].toUpperCase();
-    let B = b[key].toUpperCase();
+  return function (a, b){
+
+    // Default Values
+    let A = undefined;
+    let B = undefined;
+
+    if(a[key] !== undefined) A = a[key];
+    if(b[key] !== undefined) B = b[key];
+
     return naturalComparator(A, B);
   }
 }
--- test.js
+++ test.js
@@ -1,11 +1,46 @@
+#!/usr/bin/env -S node
 
-//const byKey = require('natural-sort-by-key');
+const assert = require('assert');
 const byKey = require('./index.js');
 
-let list = [
+const input = [
+  {},
   {flarp: '10 Baz'},
   {flarp: '100 Baz'},
+  {flarp: '11 Baz'},
+  {flarp: null},
+  {flarp: undefined},
+  {flarp: 0},
+  {flarp: 10},
+  {flarp: 11},
+  {flarp: 100},
+  {flarp: ''},
+  {flarp: ' '},
+  {flarp: '  '},
   {flarp: '20 Baz'},
-]
+  {},
+];
 
-console.log(list.sort(byKey('flarp')))
+const expected = [
+  { "flarp": "" },
+  { "flarp": " " },
+  { "flarp": "  " },
+  { "flarp": 0 },
+  { "flarp": 10 },
+  { "flarp": "10 Baz" },
+  { "flarp": 11 },
+  { "flarp": "11 Baz" },
+  { "flarp": "20 Baz" },
+  { "flarp": 100 },
+  { "flarp": "100 Baz" },
+  { "flarp": null },
+  {},
+  {flarp: undefined},
+  {},
+
+];
+
+
+const actual = input.sort(byKey('flarp'))
+assert.deepStrictEqual(actual,expected);
+//console.log(JSON.stringify(actual, null, '  '));
